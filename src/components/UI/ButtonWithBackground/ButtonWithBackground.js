fb9f517d956dd3ec7f4680660b321d9cf3eba929
dexstar <dexstaar@gmail.com>
2018-01-10 08:18:35 +0800

    Done lec 114

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import React from "react";
+import {
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Text,
+  View,
+  StyleSheet,
+  Platform
+} from "react-native";
 
-const buttonWithBackground = props => (
-    <TouchableOpacity onPress={props.onPress}>
-        <View style={[styles.button, {backgroundColor: props.color}]}>
-            <Text>{props.children}</Text>
-        </View>
-    </TouchableOpacity>
-);
+const buttonWithBackground = props => {
+  const content = (
+    <View style={[styles.button, { backgroundColor: props.color }]}>
+      <Text>{props.children}</Text>
+    </View>
+  );
+
+  if (Platform.OS === "android") {
+    return (
+      <TouchableNativeFeedback onPress={props.onPress}>
+        {content}
+      </TouchableNativeFeedback>
+    );
+  }
+
+  return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>;
+};
 
 const styles = StyleSheet.create({
   button: {
@@ -17,6 +34,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "black"
   }
-})
+});
 
 export default buttonWithBackground;
